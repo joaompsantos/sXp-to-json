266943643be37de57dcb790ef48f15ed0a56d655
Joao Santos <joao.mp.santos@ua.pt>
2018-03-03 01:51:25 +0000

    File Parameters Working

--- main.js
+++ main.js
@@ -1,24 +1,104 @@
 const fs = require('fs');
 const readline = require('readline');
 
+// Define Frequency Units
+var fscale = [
+  {name:'HZ', scale: 1},
+  {name:'KHZ', scale: 3},
+  {name:'MHZ', scale: 6},
+  {name:'GHZ', scale: 9},
+  {name:'THZ', scale: 12}
+];
 
-console.log('Starting conversion ...');
+// Define Params
+var params = [
+  {name:'S'},
+  {name:'Z'},
+  {name:'Y'},
+  {name:'H'},
+  {name:'G'}
+];
+
+// Define Format
+var format = [
+  {name:'RI'},
+  {name:'MA'},
+  {name:'DB'},
+];
+
+
+
+// Flag to indicate data starting
+var save  = 0;
+
+// Array to save usefull data
+let data=[];
+
+// JSON to hold Final data
+var vna = new Object();
+
+
+console.log('Loading file...');
 
 var rl = readline.createInterface({
-    input: fs.createReadStream(__dirname + '/example.s2p', 'utf8'),
+    input: fs.createReadStream(__dirname + '/dummy.s2p', 'utf8'),
     output: process.stdout,
     terminal: false
 });
 
-rl.on('line', l => {
-    l.charAt(0) == '!' 
-    ? console.log('yeeeee')
-    : console.log('nooooo');
+// "LOOP" through lines
+rl.on('line', (l) => {
+  if(l.charAt(0)=='#')
+    save = 1;
+
+  if(save)
+    data.push(l);
+
 });
 
+// Close Line Reader
 rl.on('close', () => {
-    console.log('Conversion ended!');
+    console.log('   Done Loading \n     -> Starting Decoding');
+
+    // Organize data into json object
+    vna = getData(data);
+
+    console.log('Organized Data : ');
+    console.log(vna);
+
 });
 
 
 
+
+// Functions
+function getData(data){
+  // Aux Json
+  aux = new Object();
+
+  // Split int params
+  re = ' ';
+  let p = data[0].split(re)
+
+  aux.fscale = fscale.filter(
+    function(fscale) {
+      return fscale.name == p[1];
+    }
+  )[0].scale;
+
+  aux.params = params.filter(
+    function(params) {
+      return params.name == p[2];
+    }
+  )[0].name;
+
+  aux.format = format.filter(
+    function(format) {
+      return format.name == p[3];
+    }
+  )[0].name;
+
+  aux.load = parseInt(p[5]);
+
+  return aux;
+}
