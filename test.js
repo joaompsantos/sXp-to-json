c2cb73e5475543e23220cfdd7c2df6abe80b3a3e
Joao Santos <joao.mp.santos@ua.pt>
2018-06-09 01:28:37 +0100

    Organized test.js

--- test.js
+++ test.js
@@ -1,17 +1,10 @@
 var s2p = require('./index.js')
 let sxp = new s2p('/ExemploRealImaginary.s2p', null)
 
-//sxp.save('test.json');
-console.log(sxp.searchFreq(856500000, 'RL'));
-console.log(sxp.ReIm(11)[0]);
-
-
-console.log(sxp.LogMag(11)[0]);
-console.log(sxp.LogMag(11)[1]);
-console.log(sxp.LogMag(11)[2]);
-console.log(sxp.LogMag(11)[3]);
-console.log(sxp.ReIm(11)[4]);
-console.log(sxp.ReIm(11)[5]);
-console.log(sxp.ReIm(11)[6]);
-console.log(sxp.ReIm(11)[7]);
-sxp.save('test1.json');
+// Save Object
+sxp.save('test.json');
+// Search for point
+console.log(sxp.searchFreq(5, 11));
+// Output format
+console.log(sxp.ReIm(11));
+console.log(sxp.LogMag(11));
